78b5b68c934a510e06f4102877c8506cfe1b505c
KantoAndrianina <kanto.andrianina@yahoo.com>
2024-07-13 14:36:18 +0300

    google analytics

--- resources/views/includes/front/head.blade.php
+++ resources/views/includes/front/head.blade.php
@@ -1,7 +1,15 @@
 <?php 
 $url = config('app.url');
 ?>
-   
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-6RRXFGB5YT"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-6RRXFGB5YT');
+    </script>
     <meta charset= "utf-8" >
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="<?php echo $url ?>/assets/bootstrap-5.3.3-dist/css/bootstrap.min.css">
